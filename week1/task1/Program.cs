bf6fda87f046bed2825dfb34b803708da29e2935
Bekzat Tolegenov <tolegenovbt@gmail.com>
2019-02-01 22:55:38 +0600

    some corrections

--- week1/task1/Program.cs
+++ week1/task1/Program.cs
@@ -12,17 +12,15 @@ namespace task1
             string[] s = Console.ReadLine().Split(); //the numbers 
             for (int i = 0; i < n; i++)
             {
-                int cnt1 = 0;
+                int cnt1 = 0;//for counting number of possible dividers without remainder
                 int k = int.Parse(s[i]);
-                
-                    for (int j = 1; j < k; j++)
-                    {
-                        if (k % j == 0)
-                            cnt1++;
-                    }
-                    if (cnt1==1)
-                        array[cnt++] = k;
-                
+                for (int j = 1; j < k; j++)
+                {
+                    if (k % j == 0)
+                    cnt1++;
+                }
+                if (cnt1 == 1)
+                    array[cnt++] = k;   
             }
             Console.WriteLine(cnt); //the first output- number of the prime integers
             for (int i = 0; i < cnt; i++) //loop to output each of the prime integers
