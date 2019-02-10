40248ef85a5665e313b03c035af4bbb1e291a6f6
Bekzat Tolegenov <tolegenovbt@gmail.com>
2019-02-11 01:32:52 +0600

    task3.1

--- week2/task3/Program.cs
+++ week2/task3/Program.cs
@@ -8,37 +8,12 @@ namespace task3
         public static void Main()
         {
             string s="";
-            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\11111\c#\labs\week2");
+            DirectoryInfo dirInfo = new DirectoryInfo(@Console.ReadLine());
             DirectoryInfo[] dirs = dirInfo.GetDirectories();
             FileSystemInfo[] fileInfos = dirInfo.GetFileSystemInfos();
-            
             FileSystemInfos(fileInfos,s);
             Console.ReadKey();
         }
-
-        //static void Dirs(DirectoryInfo[] dirs)
-        //{
-        //    for (var i = 0; i < dirs.Length; i++)
-        //    {
-        //        Console.WriteLine(dirs[i].FullName);
-
-
-        //        if (dirs[i].GetType() == typeof(DirectoryInfo))
-        //        {
-        //            var fInfos = (dirs[i] as DirectoryInfo).GetFileSystemInfos();
-        //            Dirs();
-        //        }
-        //    }
-        //}
-
-        //static void Files(FileInfo[] files)
-        //{ 
-        //    for(var i=0; i < files.Length; i++)
-        //    {
-        //        Console.WriteLine(files[i].FullName);
-        //    }
-
-        //}
 
         static void FileSystemInfos(FileSystemInfo[] fileInfos, string s)
         {
@@ -49,11 +24,9 @@ namespace task3
                 if (fileInfos[i].GetType() == typeof(DirectoryInfo))
                 {
                     s += "     ";
-                    //var fInfos = (fileInfos[i] as DirectoryInfo).GetFileSystemInfos();
                     DirectoryInfo d = new DirectoryInfo(fileInfos[i].FullName);
                     FileSystemInfo[] fs = d.GetFileSystemInfos();
                     FileSystemInfos(fs,s);
-                    
                 }
                 s=r;
             }
