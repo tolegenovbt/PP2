18a036ac08f91a7c72146a926914a971beee243e
Bekzat Tolegenov <tolegenovbt@gmail.com>
2019-02-14 22:08:10 +0600

    GG

--- week2/task3/Program.cs
+++ week2/task3/Program.cs
@@ -7,28 +7,28 @@ namespace task3
     {
         public static void Main()
         {
-            string s="";
-            DirectoryInfo dirInfo = new DirectoryInfo(@Console.ReadLine());
-            DirectoryInfo[] dirs = dirInfo.GetDirectories();
-            FileSystemInfo[] fileInfos = dirInfo.GetFileSystemInfos();
-            FileSystemInfos(fileInfos,s);
+            string s="";//string for leaving a space 
+            DirectoryInfo dirInfo = new DirectoryInfo(@Console.ReadLine());//path to a directory
+            DirectoryInfo[] dirs = dirInfo.GetDirectories();//getting  directories inside of the main directory
+            FileSystemInfo[] fileInfos = dirInfo.GetFileSystemInfos();//getting files inside of the main directory
+            FileSystemInfos(fileInfos,s);//calling the method
             Console.ReadKey();
         }
 
         static void FileSystemInfos(FileSystemInfo[] fileInfos, string s)
         {
-            for(var i=0; i<fileInfos.Length;i++)
+            for(var i=0; i<fileInfos.Length;i++)//loop for show each of files and directories inside 
             {
                 Console.WriteLine(s+fileInfos[i]);
-                string r = s;
+                string r = s;//saving space in another string
                 if (fileInfos[i].GetType() == typeof(DirectoryInfo))
                 {
-                    s += "     ";
+                    s += "     ";//adding space to previous space
                     DirectoryInfo d = new DirectoryInfo(fileInfos[i].FullName);
                     FileSystemInfo[] fs = d.GetFileSystemInfos();
-                    FileSystemInfos(fs,s);
+                    FileSystemInfos(fs,s);//if the filesystem is a directory to repeat the method
                 }
-                s=r;
+                s=r;//returning the previous space
             }
             
         }
